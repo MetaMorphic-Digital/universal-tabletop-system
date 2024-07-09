c9a16847e3be4bc9fc2977dc2ec69eed0b09de10
Joseph <JPMeehan5@Gmail.com>
2024-07-09 11:49:53 -0700

    Fundamental sheet rendering

---
+++ src/module/apps/item/itemSheet.mjs
@@ -0,0 +1,486 @@
+import { prepareActiveEffectCategories } from "../../helpers/utils.mjs";
+
+const { api, sheets } = foundry.applications;
+
+/**
+ * Extend the basic ItemSheet with some very simple modifications
+ * @extends {ItemSheetV2}
+ */
+export class UTSItemSheet extends api.HandlebarsApplicationMixin(sheets.ItemSheetV2) {
+  constructor(options = {}) {
+    super(options);
+    this.#dragDrop = this.#createDragDropHandlers();
+  }
+
+  /** @override */
+  static DEFAULT_OPTIONS = {
+    classes: ["uts", "item"],
+    actions: {
+      onEditImage: this._onEditImage,
+      viewDoc: this._viewEffect,
+      createDoc: this._createEffect,
+      deleteDoc: this._deleteEffect,
+      toggleEffect: this._toggleEffect,
+    },
+    form: {
+      submitOnChange: true,
+    },
+    // Custom property that's merged into `this.options`
+    dragDrop: [{ dragSelector: "[data-drag]", dropSelector: null }],
+  };
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  static PARTS = {
+    header: {
+      template: "systems/universal-tabletop-system/templates/item/header.hbs",
+    },
+    tabs: {
+      // Foundry-provided generic template
+      template: "templates/generic/tab-navigation.hbs",
+    },
+    properties: {
+      // Foundry-provided generic template
+      template: "templates/generic/form-fields.hbs",
+    },
+    effects: {
+      template: "systems/universal-tabletop-system/templates/shared/effects.hbs",
+    },
+  };
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  async _prepareContext(options) {
+    const context = {
+      editable: this.isEditable,
+      owner: this.isOwner,
+      limited: this.document.limited,
+      item: this.item,
+      system: this.item.system,
+      flags: this.item.flags,
+      itemFields: this.item.schema.fields,
+      config: CONFIG.UTS,
+      tabs: this._getTabs(options.parts),
+    };
+
+    return context;
+  }
+
+  /** @override */
+  async _preparePartContext(partId, context) {
+    // TODO: Come up with clever way to automatically handle enriching HTML fields
+    switch (partId) {
+      case "effects":
+        context.effects = prepareActiveEffectCategories(this.item.effects);
+        context.tab = context.tabs[partId];
+        break;
+      case "properties":
+        context.fields = this._getFields();
+        context.tab = context.tabs[partId];
+        break;
+    }
+    return context;
+  }
+
+  /**
+   * Generates the data for the generic tab navigation template
+   * @param {string[]} parts An array of named template parts to render
+   * @returns {Record<string, Partial<ApplicationTab>>}
+   * @protected
+   */
+  _getTabs(parts) {
+    // If you have sub-tabs this is necessary to change
+    const tabGroup = "primary";
+    // Default tab for first time it's rendered this session
+    if (!this.tabGroups[tabGroup]) this.tabGroups[tabGroup] = "properties";
+    return parts.reduce((tabs, partId) => {
+      const tab = {
+        cssClass: "",
+        group: tabGroup,
+        id: "",
+        icon: "",
+        // Run through localization
+        label: "UTS.Tabs.",
+      };
+      switch (partId) {
+        case "header":
+        case "tabs":
+          return tabs;
+        case "properties":
+          tab.id = "properties";
+          tab.label += "Properties";
+          break;
+        case "effects":
+          tab.id = "effects";
+          tab.label += "Effects";
+          break;
+      }
+      if (this.tabGroups[tabGroup] === tab.id) tab.cssClass = "active";
+      tabs[partId] = tab;
+      return tabs;
+    }, {});
+  }
+  /**
+   * Handles the system fields for the form-fields generic
+   */
+  _getFields() {
+    const doc = this.item;
+    const source = doc._source;
+    const systemFields = CONFIG.Item.dataModels[doc.type]?.schema.fields;
+    const fieldSets = [];
+    for (const field of Object.values(systemFields ?? {})) {
+      const path = `system.${field.name}`;
+      if (field instanceof foundry.data.fields.SchemaField) {
+        const fieldset = { fieldset: true, legend: field.label, fields: [] };
+        this.#addSystemFields(fieldset, field.fields, source, path);
+      } else {
+        fieldSets.push({ outer: { field, value: foundry.utils.getProperty(source, path) } });
+      }
+    }
+    return fieldSets;
+  }
+
+  /**
+   * Recursively add system model fields to the fieldset.
+   */
+  #addSystemFields(fieldset, schema, source, _path = "system") {
+    for (const field of Object.values(schema)) {
+      const path = `${_path}.${field.name}`;
+      if (field instanceof foundry.data.fields.SchemaField) {
+        this.#addSystemFields(fieldset, field.fields, source, path);
+      } else if (field.constructor.hasFormSupport) {
+        fieldset.fields.push({ field, value: foundry.utils.getProperty(source, path) });
+      }
+    }
+  }
+
+  /**
+   * Actions performed after any render of the Application.
+   * Post-render steps are not awaited by the render process.
+   * @param {ApplicationRenderContext} context      Prepared context data
+   * @param {RenderOptions} options                 Provided render options
+   * @protected
+   */
+  _onRender(context, options) {
+    this.#dragDrop.forEach((d) => d.bind(this.element));
+  }
+
+  /**************
+   *
+   *   ACTIONS
+   *
+   **************/
+
+  /**
+   * Handle changing a Document's image.
+   *
+   * @this UTSItemSheet
+   * @param {PointerEvent} event   The originating click event
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action]
+   * @returns {Promise}
+   * @protected
+   */
+  static async _onEditImage(event, target) {
+    const attr = target.dataset.edit;
+    const current = foundry.utils.getProperty(this.document, attr);
+    const { img } = this.document.constructor.getDefaultArtwork?.(this.document.toObject()) ?? {};
+    const fp = new FilePicker({
+      current,
+      type: "image",
+      redirectToRoot: img ? [img] : [],
+      callback: (path) => {
+        target.src = path;
+        this.document.update({ img: path });
+      },
+      top: this.position.top + 40,
+      left: this.position.left + 10,
+    });
+    return fp.browse();
+  }
+
+  /**
+   * Renders an embedded document's sheet
+   *
+   * @this UTSItemSheet
+   * @param {PointerEvent} event   The originating click event
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action]
+   * @protected
+   */
+  static async _viewEffect(event, target) {
+    const effect = this._getEffect(target);
+    effect.sheet.render(true);
+  }
+
+  /**
+   * Handles item deletion
+   *
+   * @this UTSItemSheet
+   * @param {PointerEvent} event   The originating click event
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action]
+   * @protected
+   */
+  static async _deleteEffect(event, target) {
+    const effect = this._getEffect(target);
+    await effect.delete();
+  }
+
+  /**
+   * Handle creating a new Owned Item or ActiveEffect for the actor using initial data defined in the HTML dataset
+   *
+   * @this UTSItemSheet
+   * @param {PointerEvent} event   The originating click event
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action]
+   * @private
+   */
+  static async _createEffect(event, target) {
+    const aeCls = getDocumentClass("ActiveEffect");
+    const effectData = {
+      name: aeCls.defaultName({
+        type: target.dataset.type,
+        parent: this.item,
+      }),
+    };
+    for (const [dataKey, value] of Object.entries(target.dataset)) {
+      if (["action", "documentClass"].includes(dataKey)) continue;
+      foundry.utils.setProperty(effectData, dataKey, value);
+    }
+
+    await aeCls.create(effectData, { parent: this.item });
+  }
+
+  /**
+   * Determines effect parent to pass to helper
+   *
+   * @this UTSItemSheet
+   * @param {PointerEvent} event   The originating click event
+   * @param {HTMLElement} target   The capturing HTML element which defined a [data-action]
+   * @private
+   */
+  static async _toggleEffect(event, target) {
+    const effect = this._getEffect(target);
+    await effect.update({ disabled: !effect.disabled });
+  }
+
+  /** Helper Functions */
+
+  /**
+   * Fetches the row with the data for the rendered embedded document
+   *
+   * @param {HTMLElement} target  The element with the action
+   * @returns {HTMLLIElement} The document's row
+   */
+  _getEffect(target) {
+    const li = target.closest(".effect");
+    return this.item.effects.get(li?.dataset?.effectId);
+  }
+
+  /**
+   *
+   * DragDrop
+   *
+   */
+
+  /**
+   * Define whether a user is able to begin a dragstart workflow for a given drag selector
+   * @param {string} selector       The candidate HTML selector for dragging
+   * @returns {boolean}             Can the current user drag this selector?
+   * @protected
+   */
+  _canDragStart(selector) {
+    return this.isEditable;
+  }
+
+  /**
+   * Define whether a user is able to conclude a drag-and-drop workflow for a given drop selector
+   * @param {string} selector       The candidate HTML selector for the drop target
+   * @returns {boolean}             Can the current user drop on this selector?
+   * @protected
+   */
+  _canDragDrop(selector) {
+    return this.isEditable;
+  }
+
+  /**
+   * Callback actions which occur at the beginning of a drag start workflow.
+   * @param {DragEvent} event       The originating DragEvent
+   * @protected
+   */
+  _onDragStart(event) {
+    const li = event.currentTarget;
+    if ("link" in event.target.dataset) return;
+
+    let dragData = null;
+
+    if (li.dataset.effectId) {
+      const effect = this.item.effects.get(li.dataset.effectId);
+      dragData = effect.toDragData();
+    }
+
+    if (!dragData) return;
+
+    event.dataTransfer.setData("text/plain", JSON.stringify(dragData));
+  }
+
+  /**
+   * Callback actions which occur when a dragged element is over a drop target.
+   * @param {DragEvent} event       The originating DragEvent
+   * @protected
+   */
+  _onDragOver(event) {}
+
+  /**
+   * Callback actions which occur when a dragged element is dropped on a target.
+   * @param {DragEvent} event       The originating DragEvent
+   * @protected
+   */
+  async _onDrop(event) {
+    const data = TextEditor.getDragEventData(event);
+    const item = this.item;
+    const allowed = Hooks.call("dropItemSheetData", item, this, data);
+    if (allowed === false) return;
+
+    // Handle different data types
+    switch (data.type) {
+      case "ActiveEffect":
+        return this._onDropActiveEffect(event, data);
+      case "Actor":
+        return this._onDropActor(event, data);
+      case "Item":
+        return this._onDropItem(event, data);
+      case "Folder":
+        return this._onDropFolder(event, data);
+    }
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle the dropping of ActiveEffect data onto an Actor Sheet
+   * @param {DragEvent} event                  The concluding DragEvent which contains drop data
+   * @param {object} data                      The data transfer extracted from the event
+   * @returns {Promise<ActiveEffect|boolean>}  The created ActiveEffect object or false if it couldn't be created.
+   * @protected
+   */
+  async _onDropActiveEffect(event, data) {
+    const aeCls = getDocumentClass("ActiveEffect");
+    const effect = await aeCls.fromDropData(data);
+    if (!this.item.isOwner || !effect) return false;
+
+    if (this.item.uuid === effect.parent?.uuid) return this._onEffectSort(event, effect);
+    return aeCls.create(effect, { parent: this.item });
+  }
+
+  /**
+   * Sorts an Active Effect based on its surrounding attributes
+   *
+   * @param {DragEvent} event
+   * @param {ActiveEffect} effect
+   */
+  _onEffectSort(event, effect) {
+    const effects = this.item.effects;
+    const dropTarget = event.target.closest("[data-effect-id]");
+    if (!dropTarget) return;
+    const target = effects.get(dropTarget.dataset.effectId);
+
+    // Don't sort on yourself
+    if (effect.id === target.id) return;
+
+    // Identify sibling items based on adjacent HTML elements
+    const siblings = [];
+    for (let el of dropTarget.parentElement.children) {
+      const siblingId = el.dataset.effectId;
+      if (siblingId && siblingId !== effect.id) siblings.push(effects.get(el.dataset.effectId));
+    }
+
+    // Perform the sort
+    const sortUpdates = SortingHelpers.performIntegerSort(effect, {
+      target,
+      siblings,
+    });
+    const updateData = sortUpdates.map((u) => {
+      const update = u.update;
+      update._id = u.target._id;
+      return update;
+    });
+
+    // Perform the update
+    return this.item.updateEmbeddedDocuments("ActiveEffect", updateData);
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle dropping of an Actor data onto another Actor sheet
+   * @param {DragEvent} event            The concluding DragEvent which contains drop data
+   * @param {object} data                The data transfer extracted from the event
+   * @returns {Promise<object|boolean>}  A data object which describes the result of the drop, or false if the drop was
+   *                                     not permitted.
+   * @protected
+   */
+  async _onDropActor(event, data) {
+    if (!this.item.isOwner) return false;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle dropping of an item reference or item data onto an Actor Sheet
+   * @param {DragEvent} event            The concluding DragEvent which contains drop data
+   * @param {object} data                The data transfer extracted from the event
+   * @returns {Promise<Item[]|boolean>}  The created or updated Item instances, or false if the drop was not permitted.
+   * @protected
+   */
+  async _onDropItem(event, data) {
+    if (!this.item.isOwner) return false;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle dropping of a Folder on an Actor Sheet.
+   * The core sheet currently supports dropping a Folder of Items to create all items as owned items.
+   * @param {DragEvent} event     The concluding DragEvent which contains drop data
+   * @param {object} data         The data transfer extracted from the event
+   * @returns {Promise<Item[]>}
+   * @protected
+   */
+  async _onDropFolder(event, data) {
+    if (!this.item.isOwner) return [];
+  }
+
+  /** The following pieces set up drag handling and are unlikely to need modification  */
+
+  /**
+   * Returns an array of DragDrop instances
+   * @type {DragDrop[]}
+   */
+  get dragDrop() {
+    return this.#dragDrop;
+  }
+
+  // This is marked as private because there's no real need
+  // for subclasses or external hooks to mess with it directly
+  #dragDrop;
+
+  /**
+   * Create drag-and-drop workflow handlers for this Application
+   * @returns {DragDrop[]}     An array of DragDrop handlers
+   * @private
+   */
+  #createDragDropHandlers() {
+    return this.options.dragDrop.map((d) => {
+      d.permissions = {
+        dragstart: this._canDragStart.bind(this),
+        drop: this._canDragDrop.bind(this),
+      };
+      d.callbacks = {
+        dragstart: this._onDragStart.bind(this),
+        dragover: this._onDragOver.bind(this),
+        drop: this._onDrop.bind(this),
+      };
+      return new DragDrop(d);
+    });
+  }
+}
