1077c4ad1714ebe6453b805ee9cf4eeed21af8b4
Joseph <JPMeehan5@Gmail.com>
2024-09-04 14:27:18 -0700

    Removed empty item data models folder

--- src/module/data/_module.mjs
+++ src/module/data/_module.mjs
@@ -1,5 +1,4 @@
 import * as Actor from "./actor/_module.mjs";
 import * as Combatant from "./combatant/_module.mjs";
-import * as Item from "./item/index.mjs";
 
-export {Actor, Combatant, Item};
+export {Actor, Combatant};
