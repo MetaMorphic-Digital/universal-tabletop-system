fe8ea0ee980a37d380459758959b423a24aec886
Joseph <JPMeehan5@Gmail.com>
2025-02-10 12:21:25 -0800

    Bug fixes (#11)

--- src/module/apps/combat/UTSCombatTracker.mjs
+++ src/module/apps/combat/UTSCombatTracker.mjs
@@ -4,15 +4,17 @@ export class UTSCombatTracker extends foundry.applications.sidebar.tabs.CombatTr
   async _onRender(context, options) {
     await super._onRender(context, options);
     const controls = this.element.querySelector(".encounter-controls.combat");
-    controls.insertAdjacentHTML("afterbegin", `<button class="inline-control combat-control icon fa-solid fa-user"
-      aria-label="${game.i18n.localize("UTS.Combat.AddPlayer")}"
-      type="button" data-tooltip="UTS.Combat.AddPlayer" data-action="addPlayer">
+    if (controls) {
+      controls.insertAdjacentHTML("afterbegin", `<button class="inline-control combat-control icon fa-solid fa-user"
+        aria-label="${game.i18n.localize("UTS.Combat.AddPlayer")}"
+        type="button" data-tooltip="UTS.Combat.AddPlayer" data-action="addPlayer">
       </button>`);
+    }
   }
 
   /** @override */
   async _onCombatCreate(event, target) {
-    if (Combat.TYPES.length) {
+    if (Combat.TYPES.length > 1) {
       const combat = await getDocumentClass("Combat").createDialog();
       if (combat) combat.activate({render: false});
     }
