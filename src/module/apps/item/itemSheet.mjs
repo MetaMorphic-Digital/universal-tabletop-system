b8196fd4564212335bf42b1b5abb2b0677b1aaba
Joseph <JPMeehan5@Gmail.com>
2025-03-18 19:59:06 -0700

    More Deprecations

--- src/module/apps/item/itemSheet.mjs
+++ src/module/apps/item/itemSheet.mjs
@@ -492,7 +492,7 @@ export class UTSItemSheet extends api.HandlebarsApplicationMixin(sheets.ItemShee
         dragover: this._onDragOver.bind(this),
         drop: this._onDrop.bind(this)
       };
-      return new DragDrop(d);
+      return new foundry.applications.ux.DragDrop(d);
     });
   }
 }
