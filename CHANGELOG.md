28787e61fe7083c57aad9cc951e4e181decf91f5
Joseph <JPMeehan5@Gmail.com>
2025-07-11 17:30:48 -0700

    Minor improvements

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.1.0
+
+- Active Effects with suppressed durations are now suppressed
+- Minor styling fixes
--- src/module/documents/UTSActiveEffect.mjs
+++ src/module/documents/UTSActiveEffect.mjs
@@ -2,6 +2,16 @@
  * A simple extension that adds a hook at the end of data prep
  */
 export default class UTSActiveEffect extends foundry.documents.ActiveEffect {
+  /**
+   * Automatically deactivate effects with expired durations
+   * @inheritdoc
+   */
+  get isSuppressed() {
+    if (Number.isNumeric(this.duration.remaining)) return this.duration.remaining <= 0;
+    // Checks `system.isSuppressed`
+    else return super.isSuppressed;
+  }
+
   /** @inheritdoc */
   prepareDerivedData() {
     super.prepareDerivedData();
